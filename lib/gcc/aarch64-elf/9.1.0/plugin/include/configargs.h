39c6ca0e0fd7e7b65e0f3e250e8d10fcfa761ee2
YousefAlgadri <yusufgadrie@gmail.com>
2019-05-20 12:32:08 +0700

    GCC 9.1.0 aarch64-elf
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>

---
+++ lib/gcc/aarch64-elf/9.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror CFLAGS='-O2 -fPIC -pthread' CXXFLAGS='-O2 -fPIC -pthread' --enable-languages=c --enable-shared --enable-libmpx --disable-libunwind-exceptions --enable-clocale=gnu --disable-libstdcxx-pch --disable-libssp --enable-plugin --target=aarch64-elf --prefix=/root/gcc/aarch64-elf";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
